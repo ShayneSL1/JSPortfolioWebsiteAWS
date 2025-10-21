a722bf67f418a15000cc23bcd8ed89fca6f43e3c
ShayneSL1 <ssluangvisa@gmail.com>
2025-10-21 14:47:38 -0400

    VPC resource made, including cidr block and tag resources.

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
 provider "aws" {
     region = "us-east-2"
 }
+
+#VPC
+resource "aws_vpc" "MyVPC" {
+  cidr_block = "192.168.0.0/16"
+  tags = {
+    Name = "MyVPC-tf"
+  }
+}
+
+#Subnet 0
+
+#Subnet 1
+
+#Internet Gateway
+
+#Route Table
+
+#Route Table Associations
