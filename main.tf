31d2e96216ebbe31452c57066b056435cdcbacf0
ShayneSL1 <ssluangvisa@gmail.com>
2025-10-21 14:52:14 -0400

    Subnet resources include the VPC ID to be attahed to, cidr blocks, as well as which availability zone to create them in

--- main.tf
+++ main.tf
@@ -11,11 +11,28 @@ resource "aws_vpc" "MyVPC" {
 }
 
 #Subnet 0
+resource "aws_subnet" "subnet0" {
+  vpc_id = aws_vpc.MyVPC.id
+  cidr_block = "192.168.1.0/24"
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "subnet0"
+  }
+}
 
 #Subnet 1
-
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.MyVPC.id
+  cidr_block = "192.168.2.0/24"
+  availability_zone = "us-east-2b"
+  tags = {
+    Name = "subnet1"
+  }
+}
 #Internet Gateway
-
+resource "aws_internet_gateway" "vpcIGW" {
+  vpc_id = aws_vpc.MyVPC.id
+}
 #Route Table
 
 #Route Table Associations
