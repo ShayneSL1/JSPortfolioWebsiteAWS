c60143a6abcfccf303dc6dcd4d8640f1405c4119
ShayneSL1 <ssluangvisa@gmail.com>
2025-10-22 08:11:07 -0400

    Added 3 resources to the main.tf file, which includes the resource for the s3 bucket to hold the website, the s3 bucket policy that will determine how the website will act when it is interacted with, and the CloudFront resource, which directs traffic internationally to be made available to any viewer around the globe.

--- nextjs-blog/terraform-nextjs/main.tf
+++ nextjs-blog/terraform-nextjs/main.tf
@@ -1,3 +1,85 @@
 provider "aws" {
     region = "us-east-2"
 }
+
+resource "aws_s3_bucket" "website" {
+  bucket = "your-unique-bucket-name"
+  
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+  
+  tags = {
+    Name = "Portfolio Website"
+    Environment = "Production"
+  }
+}
+
+resource "aws_s3_bucket_policy" "website_policy" {
+  bucket = aws_s3_bucket.website.id
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_cloudfront_distribution" "website_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website.website_endpoint
+    origin_id   = "S3-Website"
+    
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+  
+  enabled             = true
+  default_root_object = "index.html"
+  
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-Website"
+    
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+  
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+  
+  tags = {
+    Name = "Portfolio CloudFront"
+    Environment = "Production"
+  }
+}
+
+
