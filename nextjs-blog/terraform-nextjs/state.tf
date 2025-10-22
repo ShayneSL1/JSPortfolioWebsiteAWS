201fbea513e40b7db544026c05fd47c67c0deb30
ShayneSL1 <ssluangvisa@gmail.com>
2025-10-22 08:02:07 -0400

    S3Buckets cannot have capital letters

--- nextjs-blog/terraform-nextjs/state.tf
+++ nextjs-blog/terraform-nextjs/state.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3"{
-        bucket = "webPortfolio-terraform-state"
+        bucket = "webportfolio-terraform-state"
         key = "global/s3/backend-terraform.tfstate"
         region = "us-east-2"
         dynamodb_table = "s3-terraform-table"
